e59d532244da2a9260d4c44fe7cee51e1ce961ba
parkjunwoo0209 <ir0209@naver.com>
2025-07-08 19:36:02 +0900

    feat : 글작성 완료 페이지까지 완성 및 App.tsx 수정 PostWrite 페이지에 props를 넘겨주기 위해서 App 컴포넌트 내부로 라우터 이동시켰음 실행결과 오류 발견 x

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-
+import { useState } from "react";
 import {
   createBrowserRouter,
   RouterProvider,
@@ -8,27 +8,49 @@ import HomeLayout from "./layout/HomeLayout";
 import MainPage from "./pages/MainPage";
 import SigninPage from "./pages/SigninPage";
 import SignupPage from "./pages/SignupPage";
+import PostWrite from "./pages/PostWrite";
+import PostSuccess from "./pages/PostSuccess";
 
-const publicRoutes: RouteObject[] = [
-  {
-    path: "/",
-    element: <HomeLayout />,
-    errorElement: <div>Not Found</div>, //임시
-    children: [
-      {
-        index: true,
-        element: <MainPage />,
-      },
-      //이후 추가하시면 됩니다.
-    ],
-  },
-  { path: "/signin", element: <SigninPage /> },
-  { path: "/signup", element: <SignupPage /> },
-];
-
-const router = createBrowserRouter([...publicRoutes]);
+import type { OopsPost } from "./types/OopsList";
 
 function App() {
+  // 상태 선언
+  const [posts, setPosts] = useState<OopsPost[]>([]);
+  const [selectedStep, setSelectedStep] = useState<0 | 1 | 2>(0);
+  const [selectedPostId, setSelectedPostId] = useState<string | null>(null);
+
+  // PostWrite 컴포넌트에 필요한 상태를 props로 전달하기 위해
+  // publicRoutes를 App 내부로 이동했습니다.
+  const publicRoutes: RouteObject[] = [
+    {
+      path: "/",
+      element: <HomeLayout />,
+      errorElement: <div>Not Found</div>,
+      children: [
+        { index: true, element: <MainPage /> },
+        {
+          path: "post",
+          element: (
+            <PostWrite
+              posts={posts}
+              setPosts={setPosts}
+              selectedStep={selectedStep}
+              setSelectedStep={setSelectedStep}
+              selectedPostId={selectedPostId}
+              setSelectedPostId={setSelectedPostId}
+            />
+          ),
+        },
+      ],
+    },
+    { path: "/signin", element: <SigninPage /> },
+    { path: "/signup", element: <SignupPage /> },
+    { path: "/postsuccess", element: <PostSuccess /> },
+  ];
+
+  const router = createBrowserRouter([...publicRoutes]);
+
   return <RouterProvider router={router} />;
 }
+
 export default App;
---
+++ src/pages/PostSuccess.tsx
@@ -0,0 +1,134 @@
+import Logo from "../assets/icons/OopsLogo.svg?react";
+import Navbar from "../components/common/Navbar";
+import Footer from "../components/common/Footer";
+
+const PostSuccess = () => {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="w-[375px] h-[812px] pb-[21px] overflow-auto">
+        <Navbar />
+        {/* 첫번째 섹션 */}
+        {/* 로고 ~ 버튼 */}
+        <section className="bg-[#FFFBF8] flex flex-col items-center font-[pretendard] mt-[1px] mb-[30px] px-[20px]">
+          <Logo className="mt-[81px] w-[112px] h-[106px]" />
+          <div className="mt-[54px] w-auto h-[29px]  font-bold text-[24px]">
+            작성 완료!
+          </div>
+          <div className="mt-[12px] w-auto h-[19px]  font-medium text-[16px] ">
+            10포인트 제공 완료
+          </div>
+          {/* 버튼 */}
+          <div className="flex justify-center items-center mb-[10px] mt-[60px]">
+            <button className="bg-[#B3E378] cursor-pointer w-[335px] h-[50px] rounded-[4px] text-[14px] font-semibold">
+              방금 작성한 게시글 보러가기
+            </button>
+          </div>
+          <div className="flex justify-center items-center">
+            <button className="bg-[#1d1d1d] cursor-pointer w-[335px] h-[50px] rounded-[4px] text-[14px] text-[#b3e378] font-semibold">
+              메인 피드로 돌아가기
+            </button>
+          </div>
+        </section>
+
+        {/* 두번째 섹션 */}
+        {/* 추천 글 */}
+        <section className="bg-[#FFFBF8] flex flex-col items-center font-[pretendard] w-full mb-[20px]">
+          {/* 추천 글을 여기다 끌어오면 됨 */}
+
+          {/* 주제 */}
+          <div className="flex justify-start items-center bg-[#fbf3ec] border-b-[1px] border-[#e9e5e2] w-full h-[39px] pl-[38px] text-[16px] font-semibold">
+            작은 일 추천 글
+          </div>
+
+          <div className="flex justify-between items-center border-b-[1px] border-[#e9e5e2] w-full h-34px pl-[38px] text-[12px]">
+            내가 있잖아?
+            <span className="bg-[#B3E378] text-[#1d1d1d] flex items-center justify-center mt-[4px] mb-[6px] rounded-[20px] px-[13px] py-[6px] text-[14px] w-auto h-[24px] font-semibold">
+              웁스 중
+            </span>
+          </div>
+
+          <div className="flex justify-between items-center border-b-[1px] border-[#e9e5e2] w-full h-34px pl-[38px] text-[12px]">
+            지금 너무너무 졸린데
+            <span className="bg-[#14441a] text-[#b3e378] flex items-center justify-center mt-[4px] mb-[6px] rounded-[20px] px-[13px] py-[6px] text-[14px] w-auto h-[24px] font-semibold">
+              극복 중
+            </span>
+          </div>
+
+          <div className="flex justify-between items-center border-b-[1px] border-[#e9e5e2] w-full h-34px pl-[38px] text-[12px]">
+            알바 출근하기 싫을 때
+            <span className="bg-[#B3E378] text-[#1d1d1d] flex items-center justify-center mt-[4px] mb-[6px] rounded-[20px] px-[13px] py-[6px] text-[14px] w-auto h-[24px] font-semibold">
+              웁스 중
+            </span>
+          </div>
+
+          <div className="flex justify-between items-center border-b-[1px] border-[#e9e5e2] w-full h-34px pl-[38px] text-[12px]">
+            꿀팁 전수요
+            <span className="bg-[#14441a] text-[#b3e378] flex items-center justify-center mt-[4px] mb-[6px] rounded-[20px] px-[13px] py-[6px] text-[14px] w-auto h-[24px] font-semibold">
+              극복 중
+            </span>
+          </div>
+
+          <div className="flex justify-between items-center border-b-[1px] border-[#e9e5e2] w-full h-34px pl-[38px] text-[12px]">
+            그냥 안가면 돼...
+            <span className="bg-[#B3E378] text-[#1d1d1d] flex items-center justify-center mt-[4px] mb-[6px] rounded-[20px] px-[13px] py-[6px] text-[14px] w-auto h-[24px] font-semibold">
+              웁스 중
+            </span>
+          </div>
+
+          <div className="flex justify-between items-center border-b-[1px] border-[#e9e5e2] w-full h-34px pl-[38px] text-[12px]">
+            알바같이 하는 애한테 너무 미안하네
+            <span className="bg-[#1d1d1d] text-[#b3e378] flex items-center justify-center mt-[4px] mb-[6px] rounded-[20px] px-[13px] py-[6px] text-[14px] w-auto h-[24px] font-semibold">
+              극복 완
+            </span>
+          </div>
+
+        </section>
+
+        {/* 세번째 섹션 */}
+        {/* 베스트 글 */}
+        <section className="bg-[#FFFBF8] flex flex-col items-center font-[pretendard] w-full mb-[20px]">
+          
+          {/* 주제 */}
+          <div className="flex justify-start items-center bg-[#fbf3ec] border-b-[1px] border-[#e9e5e2] w-full h-[39px] pl-[38px] text-[16px] font-semibold">
+            베스트 Failers
+          </div>
+
+          <div className="flex justify-between items-center border-b-[1px] border-[#e9e5e2] w-full h-34px pl-[38px] text-[12px]">
+            내가 있잖아?
+            <span className="bg-[#B3E378] text-[#1d1d1d] flex items-center justify-center mt-[4px] mb-[6px] rounded-[20px] px-[13px] py-[6px] text-[14px] w-auto h-[24px] font-semibold">
+              웁스 중
+            </span>
+          </div>
+
+          <div className="flex justify-between items-center border-b-[1px] border-[#e9e5e2] w-full h-34px pl-[38px] text-[12px]">
+            지금 너무너무 졸린데
+            <span className="bg-[#B3E378] text-[#1d1d1d] flex items-center justify-center mt-[4px] mb-[6px] rounded-[20px] px-[13px] py-[6px] text-[14px] w-auto h-[24px] font-semibold">
+              극복 중
+            </span>
+          </div>
+
+          <div className="flex justify-between items-center border-b-[1px] border-[#e9e5e2] w-full h-34px pl-[38px] text-[12px]">
+            꿀팁 전수요
+            <span className="bg-[#B3E378] text-[#1d1d1d] flex items-center justify-center mt-[4px] mb-[6px] rounded-[20px] px-[13px] py-[6px] text-[14px] w-auto h-[24px] font-semibold">
+              웁스 중
+            </span>
+          </div>
+
+          <div className="flex justify-between items-center border-b-[1px] border-[#e9e5e2] w-full h-34px pl-[38px] text-[12px]">
+            그냥 안가면 돼...
+            <span className="bg-[#B3E378] text-[#1d1d1d] flex items-center justify-center mt-[4px] mb-[6px] rounded-[20px] px-[13px] py-[6px] text-[14px] w-auto h-[24px] font-semibold">
+              웁스 중
+            </span>
+          </div>
+
+        </section>
+
+        <Footer />
+        {/* 푸터 */}
+        </div>
+      </div>
+  
+  );
+};
+
+export default PostSuccess;
