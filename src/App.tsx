f5094a577defa595c0796d05648dcc523dacea9a
jangyeeunee <vkfkdto2053@gmail.com>
2025-07-10 14:37:02 +0900

    feat:main component 생성 UI 완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import './app.css'; //tailwindcss 사용하지 않아서 일단 폰트를 app.css에 넣었습니다. 협업 하면서 수정가능
 import { createBrowserRouter, RouterProvider, type RouteObject } from "react-router-dom";
 import HomeLayout from "./layout/HomeLayout";
 import MainPage from "./pages/MainPage";
---
+++ src/components/mainPage/FeedCard.tsx
@@ -0,0 +1,9 @@
+const FeedCard =() => {
+    return (
+        <>
+            {/* 공통 컴포넌트 불러와서 적용 에정*/}
+        </>
+    )
+};
+
+export default FeedCard;
