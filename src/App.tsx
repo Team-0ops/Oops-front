86c702f0ca5c1aa38ddaaddcb9bab9b7ef573afe
jangyeeunee <143832968+jangyeeunee@users.noreply.github.com>
2025-07-24 19:05:34 +0900

    Revert "Refact:글씨체 적용"

--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,10 @@
-import { Provider } from "react-redux";
-import { store } from "./store/store";
-import "./App.css";
-import { useState } from "react";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  type RouteObject,
-} from "react-router-dom";
-import HomeLayout from "./layout/HomeLayout";
-import MainPage from "./pages/MainPage";
-
-// 피드 관련 페이지
-import PostDetail from "./pages/PostDetail";
-import CategoryFeed from "./pages/CategoryFeed";
-import FavoriteFeed from "./pages/FavoriteFeed";
-import RandomFeed from "./pages/RandomFeed";
-import BestFeed from "./pages/BestFeed";
-import ExRandomFeed from "./pages/ExRandomFeed";
-// import CategoryDrawerTest from "./pages/CategoryDrawerTest";
-
-// 로그인 관련 페이지
-import SigninPage from "./pages/SigninPage";
-import SignupPage from "./pages/SignupPage";
-import FindIdPwPage from "./pages/FindIdPw";
-import TermsPage from "./pages/TermsPage";
-
-// 글쓰기, 완료, 교훈 모달
 import PostWrite from "./pages/PostWrite";
-import PostSuccess from "./pages/PostSuccess";
-import SearchPage from "./pages/SearchPage";
 
 function App() {
-  const publicRoutes: RouteObject[] = [
-    {
-      path: "/",
-      element: <HomeLayout />,
-      errorElement: <div>Not Found</div>,
-      children: [
-        { index: true, element: <MainPage /> },
-        { path: "category-feed", element: <CategoryFeed /> },
-        { path: "favorite-feed", element: <FavoriteFeed /> },
-        { path: "random-feed", element: <RandomFeed /> },
-        { path: "best-feed", element: <BestFeed /> },
-        { path: "exrandom-feed", element: <ExRandomFeed /> },
-        { path: "postsuccess", element: <PostSuccess /> },
-        { path: "postdetail", element: <PostDetail /> },
-        // { path: "drawer", element: <CategoryDrawerTest /> },
-        { path: "post", element: <PostWrite /> },
-      ],
-    },
-    { path: "/signin", element: <SigninPage /> },
-    { path: "/signup", element: <SignupPage /> },
-    { path: "/find-idpw", element: <FindIdPwPage /> },
-    { path: "/terms", element: <TermsPage /> },
-    { path: "/search", element: <SearchPage /> },
-  ];
-
-  const router = createBrowserRouter([...publicRoutes]);
-
   return (
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
+    <>
+      <PostWrite />
+    </>
   );
 }
 
--- src/components/mainPage/FeedCard.tsx
+++
@@ -1,9 +0,0 @@
-const FeedCard =() => {
-    return (
-        <>
-            {/* 공통 컴포넌트 불러와서 적용 에정*/}
-        </>
-    )
-};
-
-export default FeedCard;
