2b3bd261002fd2aade5136d0f517a07cc0c49bfc
jangyeeunee <vkfkdto2053@gmail.com>
2025-07-24 15:01:57 +0900

    refact: component add

---
+++ src/components/SearchPage/SearchInput.tsx
@@ -0,0 +1,16 @@
+const SearchInput = () => {
+  return (
+    <>
+      <input
+        type="text"
+        placeholder="인간"
+        className="w-full h-12 shrink-0 px-4 text-sm text-gray-800 
+             bg-[#FFFBF8] border border-[#F6EBE6] rounded 
+             shadow-[inset_0_0_5.4px_0_rgba(0,0,0,0.25)] 
+             focus:outline-none placeholder:text-gray-400"
+      />
+    </>
+  );
+};
+
+export default SearchInput;
