c8a6c7191ce8047a3b672fcd38c85d5152b7bf05
jangyeeunee <vkfkdto2053@gmail.com>
2025-07-27 00:27:00 +0900

    feat:wiki 검색 후 UI 완성(수정예정인 부분 제외)

--- src/pages/FailWiki.tsx
+++ src/pages/FailWiki.tsx
@@ -1,9 +1,17 @@
+import { useState } from "react";
 import WikiCategoryCard from "../components/FailWiki/WikiCategoryCard";
 import WikiHead from "../components/FailWiki/WikiHead";
 import WikiInput from "../components/FailWiki/WikiInput";
 import WikiKeyword from "../components/FailWiki/WikiKeyword";
+import WikiResult from "../components/FailWiki/WikiResult";
 
 const FailWiki = () => {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleEnterSubmit = (value: string) => {
+    setInputValue(value);
+  };
+
   return (
     <>
       <div className="flex flex-col justify-center gap-[20px]">
@@ -13,19 +21,34 @@ const FailWiki = () => {
         </div>
         <div className="flex flex-col justify-center gap-[20px] px-[18px]">
           <div>
-            <WikiInput />
+            <WikiInput onSubmit={handleEnterSubmit} />
           </div>
-          <div className="flex flex-col justify-center gap-[32px]">
-            <div>
-              <WikiKeyword />
+          {inputValue ? (
+            <>
+              <div>{/*빈 공간*/}</div>
+              <WikiResult />
+            </>
+          ) : (
+            <div className="flex flex-col justify-center gap-[32px]">
+              <div>
+                <WikiKeyword />
+              </div>
+              <div className="grid grid-cols-2 justify-items-start gap-y-[10px]">
+                <div className="justify-self-start">
+                  <WikiCategoryCard />
+                </div>
+                <div className="justify-self-end">
+                  <WikiCategoryCard />
+                </div>
+                <div className="justify-self-start">
+                  <WikiCategoryCard />
+                </div>
+                <div className="justify-self-end">
+                  <WikiCategoryCard />
+                </div>
+              </div>
             </div>
-            <div className="grid grid-cols-2 justify-items-center gap-y-[10px] ">
-              <WikiCategoryCard />
-              <WikiCategoryCard />
-              <WikiCategoryCard />
-              <WikiCategoryCard />
-            </div>
-          </div>
+          )}
         </div>
       </div>
     </>
