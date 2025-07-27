0983c74e0d30abc9dc7ee91eb4c2640ac3b7268d
jangyeeunee <vkfkdto2053@gmail.com>
2025-07-28 02:13:16 +0900

    feat: feed 보러가기 버튼적용

--- src/components/mainPage/BestFailerList.tsx
+++ src/components/mainPage/BestFailerList.tsx
@@ -1,94 +1,98 @@
+import { Link } from "react-router-dom";
 import PostCard from "../common/PostCard";
 
 const BestFailerList = () => {
-    return(
-        <>
-            <div className="flex flex-col w-full justify-center items-center gap-[16px]">
-                <div className="flex w-full justify-between items-center">
-                    <h1 className="flex text-[20px] font-semibold">베스트 Failer</h1>
-                    <button className="flex w-[75px] h-[30px] py-[3px] justify-center items-center rounded-[20px] border-[1px] border-[#E6E6E6]">
-                        <span className="text-[#4E4E4E] text-[14px] font-medium leading-[140%]">보러가기</span>
-                    </button>
-                </div>
-                <div className="flex flex-col w-full justify-center items-center gap-[16px]">
-                        <PostCard
-                            title="노래 실패담"
-                            content="노래를 부르다가 음이탈을 해서 망한 경험담입니다."
-                            imageUrl="https://example.com/image.jpg"
-                            likes={10}
-                            comments={5}
-                            views={100}
-                            category="노래"
-                        />
-                        <PostCard
-                            title="노래 실패담"
-                            content="노래를 부르다가 음이탈을 해서 망한 경험담입니다."
-                            imageUrl="https://example.com/image.jpg"
-                            likes={10}
-                            comments={5}
-                            views={100}
-                            category="노래"
-                        />
-                        <PostCard
-                            title="노래 실패담"
-                            content="노래를 부르다가 음이탈을 해서 망한 경험담입니다."
-                            imageUrl="https://example.com/image.jpg"
-                            likes={10}
-                            comments={5}
-                            views={100}
-                            category="노래"
-                        />
-                        <PostCard
-                            title="노래 실패담"
-                            content="노래를 부르다가 음이탈을 해서 망한 경험담입니다."
-                            imageUrl="https://example.com/image.jpg"
-                            likes={10}
-                            comments={5}
-                            views={100}
-                            category="노래"
-                        />
-                        <PostCard
-                            title="노래 실패담"
-                            content="노래를 부르다가 음이탈을 해서 망한 경험담입니다."
-                            imageUrl="https://example.com/image.jpg"
-                            likes={10}
-                            comments={5}
-                            views={100}
-                            category="노래"
-                        />
-                        <PostCard
-                            title="노래 실패담"
-                            content="노래를 부르다가 음이탈을 해서 망한 경험담입니다."
-                            imageUrl="https://example.com/image.jpg"
-                            likes={10}
-                            comments={5}
-                            views={100}
-                            category="노래"
-                        />
-                        <PostCard
-                            title="노래 실패담"
-                            content="노래를 부르다가 음이탈을 해서 망한 경험담입니다."
-                            imageUrl="https://example.com/image.jpg"
-                            likes={10}
-                            comments={5}
-                            views={100}
-                            category="노래"
-                        />
-                        <PostCard
-                            title="노래 실패담"
-                            content="노래를 부르다가 음이탈을 해서 망한 경험담입니다."
-                            imageUrl="https://example.com/image.jpg"
-                            likes={10}
-                            comments={5}
-                            views={100}
-                            category="노래"
-                        />
-
-                </div>
-            </div>
-        </>
-    );
-
-}
+  return (
+    <>
+      <div className="flex flex-col w-full justify-center items-center gap-[16px]">
+        <div className="flex w-full justify-between items-center">
+          <h1 className="flex text-[20px] font-semibold">베스트 Failer</h1>
+          <Link
+            to="/best-feed"
+            className="flex w-[75px] h-[30px] py-[3px] justify-center items-center rounded-[20px] border-[1px] border-[#E6E6E6]"
+          >
+            <span className="text-[#4E4E4E] text-[14px] font-medium leading-[140%]">
+              보러가기
+            </span>
+          </Link>
+        </div>
+        <div className="flex flex-col w-full justify-center items-center gap-[16px]">
+          <PostCard
+            title="노래 실패담"
+            content="노래를 부르다가 음이탈을 해서 망한 경험담입니다."
+            imageUrl="https://example.com/image.jpg"
+            likes={10}
+            comments={5}
+            views={100}
+            category="노래"
+          />
+          <PostCard
+            title="노래 실패담"
+            content="노래를 부르다가 음이탈을 해서 망한 경험담입니다."
+            imageUrl="https://example.com/image.jpg"
+            likes={10}
+            comments={5}
+            views={100}
+            category="노래"
+          />
+          <PostCard
+            title="노래 실패담"
+            content="노래를 부르다가 음이탈을 해서 망한 경험담입니다."
+            imageUrl="https://example.com/image.jpg"
+            likes={10}
+            comments={5}
+            views={100}
+            category="노래"
+          />
+          <PostCard
+            title="노래 실패담"
+            content="노래를 부르다가 음이탈을 해서 망한 경험담입니다."
+            imageUrl="https://example.com/image.jpg"
+            likes={10}
+            comments={5}
+            views={100}
+            category="노래"
+          />
+          <PostCard
+            title="노래 실패담"
+            content="노래를 부르다가 음이탈을 해서 망한 경험담입니다."
+            imageUrl="https://example.com/image.jpg"
+            likes={10}
+            comments={5}
+            views={100}
+            category="노래"
+          />
+          <PostCard
+            title="노래 실패담"
+            content="노래를 부르다가 음이탈을 해서 망한 경험담입니다."
+            imageUrl="https://example.com/image.jpg"
+            likes={10}
+            comments={5}
+            views={100}
+            category="노래"
+          />
+          <PostCard
+            title="노래 실패담"
+            content="노래를 부르다가 음이탈을 해서 망한 경험담입니다."
+            imageUrl="https://example.com/image.jpg"
+            likes={10}
+            comments={5}
+            views={100}
+            category="노래"
+          />
+          <PostCard
+            title="노래 실패담"
+            content="노래를 부르다가 음이탈을 해서 망한 경험담입니다."
+            imageUrl="https://example.com/image.jpg"
+            likes={10}
+            comments={5}
+            views={100}
+            category="노래"
+          />
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default BestFailerList;
--- src/components/mainPage/FavoritesCategoryList.tsx
+++ src/components/mainPage/FavoritesCategoryList.tsx
@@ -1,25 +1,33 @@
-import Star from '../../assets/icons/star.svg?react';
+import { Link } from "react-router-dom";
+import Star from "../../assets/icons/star.svg?react";
 import FeedCard from "./FeedCard";
 
 const FavoritesCategoryList = () => {
-    return(
-        <>
-            <div className="flex flex-col w-full justify-center items-center gap-[16px]">
-                <div className="flex w-full justify-between items-center">
-                    <div className="flex gap-[4px] items-center">
-                    <Star />
-                    <h1 className="flex text-[20px] font-semibold">즐겨찾기한 카테고리</h1>
-                    </div>
-                    <button className="flex w-[75px] h-[30px] py-[3px] justify-center items-center rounded-[20px] border-[1px] border-[#E6E6E6]">
-                        <span className="text-[#4E4E4E] text-[14px] font-medium leading-[140%]">보러가기</span>
-                    </button>
-                </div>
-                <div className="flex justify-center items-center">
-                        <FeedCard />
-                </div>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className="flex flex-col w-full justify-center items-center gap-[16px]">
+        <div className="flex w-full justify-between items-center">
+          <div className="flex gap-[4px] items-center">
+            <Star />
+            <h1 className="flex text-[20px] font-semibold">
+              즐겨찾기한 카테고리
+            </h1>
+          </div>
+          <Link
+            to="/favorite-feed"
+            className="flex w-[75px] h-[30px] py-[3px] justify-center items-center rounded-[20px] border-[1px] border-[#E6E6E6]"
+          >
+            <span className="text-[#4E4E4E] text-[14px] font-medium leading-[140%]">
+              보러가기
+            </span>
+          </Link>
+        </div>
+        <div className="flex justify-center items-center">
+          <FeedCard />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default FavoritesCategoryList;
