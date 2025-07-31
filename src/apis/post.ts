0ebef09d55c19b25cb852a6cd168afb1e8855e18
jangyeeunee <vkfkdto2053@gmail.com>
2025-07-31 19:17:56 +0900

    feat: main페이지 배너 이외에 연동 완료

--- src/apis/post.ts
+++ src/apis/post.ts
@@ -6,18 +6,14 @@ import { axiosInstance } from "./axios";
 
 export const getPostListInMainPage =
   async (): Promise<ResponseMainPostListDTO> => {
-    const { data } = await axiosInstance.get(
-      "http://localhost:3000/api/feeds/home/first"
-    );
+    const { data } = await axiosInstance.get("/feeds/home/first");
     console.log(data);
     return data;
   };
 
 export const getPostListCategory =
   async (): Promise<ResponseCategoryPostListDTO> => {
-    const { data } = await axiosInstance.get(
-      "http://localhost:3000/api/feeds/home/later"
-    );
+    const { data } = await axiosInstance.get("/feeds/home/later");
     console.log(data);
     return data;
   };
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
   plugins: [tailwindcss(), svgr()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://15.164.217.202:8080", // 실제 API 서버 주소
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 });
