75386a4509a022ea81fa62f3332bf1e8e1434267
노승민 <daniel939015@gmail.com>
2025-08-01 20:46:52 +0900

    mock데이터 삽입

--- src/pages/LuckyDrawPage/luckyDrawApi.ts
+++ src/pages/LuckyDrawPage/luckyDrawApi.ts
@@ -1,20 +1,16 @@
 // luckyDrawApi.ts
-export const getUserProfile = async () => {
-  return {
-    isSuccess: true,
-    result: {
-      point: 220, // mock 포인트
-    },
-  };
-};
+import axios from "axios";
+import { axiosInstance } from "../../apis/axios";
 
 export const requestLuckyDraw = async () => {
-  return {
-    isSuccess: true,
-    result: {
-      id: 1,
-      name: "행운의 부적",
-      imageUrl: "https://example.com/lucky-charm.png",
-    },
-  };
+
+
+  const res = await axiosInstance.post(
+    "/lucky-draw",
+    {},
+    {
+    }
+  );
+
+  return res.data;
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+	import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 import svgr from "vite-plugin-svgr";
+
 export default defineConfig({
-  plugins: [
-    tailwindcss(), svgr()
-  ],
-})
+  plugins: [tailwindcss(), svgr()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://15.164.217.202:8080", // 실제 API 서버 주소
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
