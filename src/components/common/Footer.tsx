a04975598245c6c173105aa5c0b9b7ae8403d2ef
DOL-DOM <yeojiwon0405@gmail.com>
2025-08-15 00:38:32 +0900

    refact:푸터 클릭하면 해당 노션페이지 이동하도록 수정

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -8,9 +8,30 @@ const Footer = () => {
           <p>멤버 소개</p>
         </div>
         <div className="flex flex-col gap-[6px]">
-          <p>이용약관</p>
-          <p>개인정보 처리방침</p>
-          <p>신고가이드</p>
+          <a
+            href="https://humane-trollius-aaf.notion.site/Oops-2267949238998078a1f4db2c51db7cac?source=copy_link"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:underline"
+          >
+            이용약관
+          </a>
+          <a
+            href="https://humane-trollius-aaf.notion.site/Oops-22679492389980c7abdbfc05cca7313f?source=copy_link"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:underline"
+          >
+            개인정보 처리방침
+          </a>
+          <a
+            href="https://humane-trollius-aaf.notion.site/Oops-24f794923899800cbae1f5780c547eb3?source=copy_link"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:underline"
+          >
+            신고가이드
+          </a>
         </div>
       </div>
     </footer>
