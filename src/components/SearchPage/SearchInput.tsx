912fa3cd5af403a2039dd5f741f140a94877d503
jangyeeunee <vkfkdto2053@gmail.com>
2025-08-15 21:16:59 +0900

    refact: 검색페이지 place holder 수정

--- src/components/SearchPage/SearchInput.tsx
+++ src/components/SearchPage/SearchInput.tsx
@@ -20,7 +20,7 @@ const SearchInput = ({ onSubmit }: SearchInputProps) => {
         value={value}
         onChange={(e) => setValue(e.target.value)}
         onKeyDown={handleKeyDown}
-        placeholder="인간"
+        placeholder="무엇이든 검색해보세요!"
         className="w-full h-[48px] shrink-0 px-4 text-sm text-gray-800 
              bg-[#FFFBF8] border border-[#F6EBE6] rounded 
              shadow-[inset_0_0_5.4px_0_rgba(0,0,0,0.25)] 
