cd5aae351639c5a47c29e47811ab5812062924e0
parkjunwoo0209 <ir0209@naver.com>
2025-08-15 17:30:25 +0900

    랜덤주제 카테고리 선택후 글 작성 완료

--- src/types/OopsList.ts
+++ src/types/OopsList.ts
@@ -2,7 +2,7 @@ export type OopsPost = {
   title: string;
   content: string;
   situation: "OOPS" | "OVERCOMING" | "OVERCOME";
-  categoryId: number;
+  categoryId: number | null;
   topicId: number | null;
   previousPostId?: number | null;
   wantedCommentTypes?: ("ADVICE" | "EMPATHY")[];
